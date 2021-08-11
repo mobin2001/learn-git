0c7de48499da6d5e5ad6c0ba4c22672a01f64a6e
Mobin <mb.davaji@yahoo.com>
2021-08-12 00:19:00 +0430

    program fixed

--- sqrt.py
+++ sqrt.py
@@ -4,6 +4,7 @@ javab = dict()
 for x in range (0,tedad_adad):
     adad = float(input())
     adad = math.sqrt(adad)
-    javab[x] = "5.8f%"%adad
+    print(adad)
+    javab[x] = "%5.8f"%(adad)
 for y in range (0,tedad_adad):
-    print(javab[y][4])
+    print(javab[y][:-4])
