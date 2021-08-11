547f355258750a1c07838ca9d2573762ff399645
Mobin <mb.davaji@yahoo.com>
2021-08-12 00:24:03 +0430

    bug fixed

--- sqrt.py
+++ sqrt.py
@@ -4,7 +4,9 @@ javab = dict()
 for x in range (0,tedad_adad):
     adad = float(input())
     adad = math.sqrt(adad)
-    print(adad)
+    #jazr adad ra ta 8 ragam ashar zakhire mikonad
     javab[x] = "%5.8f"%(adad)
+
 for y in range (0,tedad_adad):
     print(javab[y][:-4])
+#jazr ra ta 4 ragam ashar bdon round kardan chap mikonad
